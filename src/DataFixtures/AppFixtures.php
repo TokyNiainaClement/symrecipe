4645a1c7583c7e678e02bc3e0ffe897c5db8dd36
TokyNiainaClement <tokiniainartc@gmail.com>
2026-04-26 20:47:09 +0300

    📝 Added fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,36 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Ingredient;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Faker\Generator;
 
 class AppFixtures extends Fixture
 {
+    /**
+     *
+     * @var Generator
+     */
+    private Generator $faker;
+
+    public function __construct()
+    {
+        $this->faker = Factory::create("fr_FR");
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for ($i = 1; $i <= 50; $i++) {
+            $ingredient = new Ingredient();
+            $ingredient->setName($this->faker->word())
+            ->setPrice(mt_rand(0, 100));
 
+            // Persister-na le objet crée otran'ny manao git add .
+            $manager->persist($ingredient);
+        }
+        // De avy eo pusher-na any amin'ny base de donnée
         $manager->flush();
     }
 }
